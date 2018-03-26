15bf9c022ba8dc7babc83b0a871dc73f13ef8106
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-03-26 00:41:13 -0400

    upload

--- track.sh
+++ track.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-ps -eo size,pid,user,command --sort -size | awk '{ hr=$1/1024 ; printf("%13.2f Mb ",hr) } { for ( x=4 ; x<=NF ; x++ ) { printf("%s ",$x) } print "" }' |cut -d "" -f2 | cut -d "-" -f1 | grep apriori >> memtrack.txt
+while true
+do 
+    ps -eo size,pid,user,command --sort -size | awk '{ hr=$1/1024 ; printf("%13.2f Mb ",hr) } { for ( x=4 ; x<=NF ; x++ ) { printf("%s ",$x) } print "" }' |cut -d "" -f2 | cut -d "-" -f1 | grep apriori >> memtrack.txt
+
+    sleep 1
+done
